58b983f057e60bd41f3d0ffb6c7703e9588970fd
jacuchilla <jcuchilla.csupomona@gmail.com>
2017-02-26 20:34:06 -0800

    Tech stack spacing increase

--- src/app/sidenavbar/sidenavbar.component.ts
+++ src/app/sidenavbar/sidenavbar.component.ts
@@ -13,8 +13,8 @@ export class SidenavbarComponent implements OnInit {
     // console.log(this.elRef.nativeElement.querySelector('#sidenavbar'))
     if (event.target.innerWidth <= 703) {
       // console.log(this.sideNavBar._results[0]._opened)
-      console.log(this.sideNavBar._results[0])
-      console.log(this.sideNavBar._results[0].mode)
+      // console.log(this.sideNavBar._results[0])
+      // console.log(this.sideNavBar._results[0].mode)
 
       this.sideNavBar._results[0]._opened = false;
       this.sideNavBar._results[0].mode = "over";
