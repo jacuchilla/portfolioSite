4c67d369afbc99aedef383d24318e2cb548c0a5e
jacuchilla <jcuchilla.csupomona@gmail.com>
2017-02-26 19:41:36 -0800

    Sidebar mode made responsive

--- src/app/sidenavbar/sidenavbar.component.ts
+++ src/app/sidenavbar/sidenavbar.component.ts
@@ -11,13 +11,20 @@ export class SidenavbarComponent implements OnInit {
   onResize(event) {
     // console.log(event.target.innerWidth === 703)
     // console.log(this.elRef.nativeElement.querySelector('#sidenavbar'))
-    if(event.target.innerWidth <= 703) {
-      console.log(this.sideNavBar._results[0]._opened)
+    if (event.target.innerWidth <= 703) {
+      // console.log(this.sideNavBar._results[0]._opened)
+      console.log(this.sideNavBar._results[0])
+      console.log(this.sideNavBar._results[0].mode)
+
       this.sideNavBar._results[0]._opened = false;
+      this.sideNavBar._results[0].mode = "over";
       // this.elRef.nativeElement.querySelector('#sidenavbar')
       // console.log(this.elRef.nativeElement.querySelector('#sidenavbar'))
       // console.log(this.elRef.nativeElement.querySelector('#sidenavbar'))
       // console.log(this.elRef.nativeElement.getAttribute('sidebar'))
+    } 
+    else {
+      this.sideNavBar._results[0].mode = "side";
     }
   } 
 
