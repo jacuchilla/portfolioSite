a6ea9279d02aef5736ae71a2166b297a41d1b1f9
jacuchilla <jcuchilla.csupomona@gmail.com>
2017-03-03 15:36:50 -0800

    Testing route

--- server.js
+++ server.js
@@ -36,7 +36,7 @@ const app = express();
 app.use(express.static(__dirname + '/dist'));
 
 app.get("/expertise", function(req, res) {
-  res.send(console.log('works!'))
+  express.static(__dirname + '/dist');
 })
 
 // Start the app by listening on the default
